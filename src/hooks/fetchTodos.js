9393959f7fe54b2ee248cac3719453d283909a3e
Diogo Soares <diogo.soares05@gmail.com>
2024-09-12 23:37:04 +0200

    created hook to display page content after reloading

---
+++ src/hooks/fetchTodos.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { useAuth } from '../context/Authcontext'
+
+export default function useFetchTodos() {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [todos, setTodos] = useState(null)
+
+  const { currentUser } = useAuth()
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const docRef = doc(db, 'users', currentUser.uid)
+        const docSnap = await getDoc(docRef)
+      } catch (err) {
+      } finally {
+      }
+    }
+  }, [])
+
+  return { loading, error, todos }
+}
